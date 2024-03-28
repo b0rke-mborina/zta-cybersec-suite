21af6e6a0b396b675ef59ca63ebfb54c608a7dac
b0rke-mborina <mborina@net.hr>
2024-03-28 14:06:51 +0100

    Log database, utility functions
    - added database for logs
    - added utility functions for sending requests
    - added utility function for logging
    - added utility functions for finding database path
    - added utility functions for generation algorithms
    - added utility functions for verification algorithms

---
+++ app2AuthenticationGenerator/utilityFunctions.py
@@ -0,0 +1,58 @@
+import aiohttp
+import asyncio
+import aiosqlite
+import json
+import os.path
+
+async def request(session, method, url, data):
+	async with session.request(method = method, url = url, data = json.dumps(data)) as response:
+			return await response.json()
+
+async def sendRequest(method, url, reqData):
+	async with aiohttp.ClientSession() as session:
+		task = request(session, method, url, reqData)
+		result = await asyncio.gather(task)
+		return result
+
+async def log(dataItem, dbName):
+	async with aiosqlite.connect(getDbPath(dbName)) as db:
+		await db.execute(
+			"INSERT INTO Log (timestamp, level, logger_source, user_id, request, response, error_message) VALUES (?, ?, ?, ?, ?, ?, ?)",
+			(
+				dataItem.timestamp,
+				dataItem.level,
+				dataItem.logger_source,
+				dataItem.user_id,
+				dataItem.request,
+				dataItem.response,
+				dataItem.error_message
+			)
+		)
+		await db.commit()
+
+def getDbPath(dbFilename):
+	baseDir = os.path.dirname(os.path.abspath(__file__))
+	dbPath = os.path.join(baseDir, dbFilename)
+	return dbPath
+
+def generateAPIKey():
+	pass
+
+def generateOAuth():
+	pass
+
+def generateJWT():
+	pass
+
+def verifyApiKey():
+	pass
+
+def verifyOAuth():
+	pass
+
+def verifyJWT():
+	pass
+
+
+
+
