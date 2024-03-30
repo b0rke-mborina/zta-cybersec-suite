415e273f457706f9e13810e4c74bd65597ed9499
b0rke-mborina <mborina@net.hr>
2024-03-30 11:37:47 +0100

    App 2 generation part 2
    - implemented oauth2 token generation in generation microservice
    - implemented jwt generation in generation microservice

--- app2AuthenticationGenerator/microservice1Generation.py
+++ app2AuthenticationGenerator/microservice1Generation.py
@@ -57,17 +57,79 @@ async def generatorAPIKey():
 async def generatorOAuth2():
 	oauth2Token = generateOAuth2()
 	print(oauth2Token)
-	currentTime = datetime.datetime.now().isoformat()
+	currentTime = datetime.datetime.now()
 	oauth2TokenData = {
-
+		"auth_type": "oauth2_token",
+		"token_key": oauth2Token,
+		"expires": (currentTime + datetime.timedelta(days=14)).isoformat(),
+		"user_id": 1
 	}
 
-	return { "generation": "success", "oauth2_token": oauth2Token }
+	response = { "generation": "success", "oauth2_token": oauth2Token }
+
+	storageResult = await sendRequest(
+		"post",
+		"http://127.0.0.1:8012/auth-generator/data-new",
+		oauth2TokenData
+	)
+	if storageResult[0].get("saving_info") != "success":
+		raise HTTPException(500)
+
+	loggingResult = await sendRequest(
+		"post",
+		"http://127.0.0.1:8013/auth-generator/logging",
+		{
+			"timestamp": currentTime.isoformat(),
+			"level": "INFO",
+			"logger_source": 1,
+			"user_id": 1,
+			"request": "",
+			"response": str(response),
+			"error_message": ""
+		}
+	)
+	if loggingResult[0].get("logging") != "success":
+		raise HTTPException(500)
+
+	return response
 
 @app.get("/auth-generator/generate/jwt")
 async def generatorJWT():
-	jwtToken = generateJWT()
-	print(jwtToken)
+	jwToken = generateJWT()
+	print(jwToken)
+	currentTime = datetime.datetime.now()
+	oauth2TokenData = {
+		"auth_type": "jwt",
+		"token_key": jwToken,
+		"expires": (currentTime + datetime.timedelta(days=14)).isoformat(),
+		"user_id": 1,
+		"secret": "SECRET_PLACEHOLDER"
+	}
+
+	response = { "generation": "success", "oauth2_token": jwToken }
+
+	storageResult = await sendRequest(
+		"post",
+		"http://127.0.0.1:8012/auth-generator/data-new",
+		oauth2TokenData
+	)
+	if storageResult[0].get("saving_info") != "success":
+		raise HTTPException(500)
+
+	loggingResult = await sendRequest(
+		"post",
+		"http://127.0.0.1:8013/auth-generator/logging",
+		{
+			"timestamp": currentTime.isoformat(),
+			"level": "INFO",
+			"logger_source": 1,
+			"user_id": 1,
+			"request": "",
+			"response": str(response),
+			"error_message": ""
+		}
+	)
+	if loggingResult[0].get("logging") != "success":
+		raise HTTPException(500)
 	
-	
-	return { "generation": "success", "jwt_token": jwtToken }
+	return { "generation": "success", "jwtoken": jwToken }
