8ad49fc683057c55d641c5f36ae7d2ac18a98e82
b0rke-mborina <mborina@net.hr>
2024-04-12 12:54:18 +0200

    App 5 logging
    - implemented logging microservice in app 5
    - added logging database to app 5
    - added utility functions for logging and requests

--- app5PasswordStorageAndVerification/microservice5Logging.py
+++ app5PasswordStorageAndVerification/microservice5Logging.py
@@ -1,10 +1,52 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, model_validator, validator
+import json
+import datetime
+from enum import Enum
+from .utilityFunctions import log
 
 
 app = FastAPI()
 
 
-@app.get("/password/logging")
-async def logging():
-	return {"status": "OK"}
+class Level(str, Enum):
+	INFO = "INFO"
+	DEBUG = "DEBUG"
+	WARN = "WARN"
+	ERROR = "ERROR"
+	FATAL = "FATAL"
 
+class Data(BaseModel):
+	timestamp: str
+	level: Level
+	logger_source: int
+	user_id: int
+	request: str
+	response: str = ""
+	error_message: str = ""
+
+	@validator("timestamp")
+	def validateISO8601Timestamp(cls, v):
+		try:
+			datetime.datetime.fromisoformat(v)
+		except ValueError:
+			raise ValueError("Timestamp must be in ISO 8601 format")
+		return v
+	
+	@model_validator(mode='before')
+	@classmethod
+	def to_py_dict(cls, data):
+		return json.loads(data)
+
+@app.exception_handler(Exception)
+async def exceptionHandler(request, exc):
+	return JSONResponse(
+		status_code = 500,
+		content = { "logging": "failure", "error_message": "Unexpected error occured." },
+	)
+
+@app.post("/password/logging", status_code = 200)
+async def logging(data: Data):
+	await log(data, "app5Logs.db")
+	return { "logging": "success" }
