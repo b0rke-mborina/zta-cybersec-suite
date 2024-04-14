c6af7b73e7299b8dbc54d50364c17772e7056fb1
b0rke-mborina <mborina@net.hr>
2024-04-14 20:39:09 +0200

    App 5 request validation
    - implemented request validation in app 5 (verification microservice, reset and recovery microservice)

--- app5PasswordStorageAndVerification/microservice2Verification.py
+++ app5PasswordStorageAndVerification/microservice2Verification.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
+class Data(BaseModel):
+	username: str
+	password: str
+
 @app.get("/password/verify")
-async def verification():
+async def verification(data: Data):
 	return {"status": "OK"}
--- app5PasswordStorageAndVerification/microservice3ResetAndRecovery.py
+++ app5PasswordStorageAndVerification/microservice3ResetAndRecovery.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
+class Data(BaseModel):
+	username: str
+	current_password: str
+	new_password: str
+
 @app.get("/password/reset")
-async def reset():
+async def reset(data: Data):
 	return {"status": "OK"}
