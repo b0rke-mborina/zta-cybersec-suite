09bf1a0fb1a7916e706a4badc2bde892ddbe43ee
b0rke-mborina <mborina@net.hr>
2024-04-23 09:25:29 +0200

    App 6 improvement
    - added request validation error handler to sharing microservice

--- app6FileStorage/microservice1Sharing.py
+++ app6FileStorage/microservice1Sharing.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import datetime
@@ -21,6 +22,24 @@ class DataStore(BaseModel):
 
 class DataRetrieve(BaseModel):
 	filename: str
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+	dataForLoggingUnsuccessfulRequest = {
+		"timestamp": datetime.datetime.now().isoformat(),
+		"level": "INFO",
+		"logger_source": 1,
+		"user_id": 1,
+		"request": f"Request: {request.url} {request.method} {request.headers} {request.query_params} {request.path_params} {await request.body()}",
+		"response": "",
+		"error_message": f"Unsuccessful request due to a Request Validation error. {exc}"
+	}
+	await sendRequest("post", "http://127.0.0.1:8054/file/logging", dataForLoggingUnsuccessfulRequest)
+
+	return JSONResponse(
+		status_code = 400,
+		content = { "storage": "failure", "error_message": "Input invalid." },
+	)
 
 @app.exception_handler(HTTPException)
 async def exceptionHandler(request, exc):
