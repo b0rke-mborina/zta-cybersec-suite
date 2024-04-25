70bdcddae58c9848495dfa7cb72c7c8bbe5e5125
b0rke-mborina <mborina@net.hr>
2024-04-25 12:03:23 +0200

    App 7 access control
    - implemented access control microservice in app 7
    - added database for access control
    - added helper functions for access control

--- app7DataMasking/microservice4AccessControl.py
+++ app7DataMasking/microservice4AccessControl.py
@@ -1,9 +1,35 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, model_validator
+import json
+from enum import Enum
+from .utilityFunctions import checkIfUserAllowed
 
 
 app = FastAPI()
 
 
+class Role(str, Enum):
+	USER = "user"
+	ADMIN = "admin"
+
+class Data(BaseModel):
+	user_id: int
+	role: Role
+	
+	@model_validator(mode='before')
+	@classmethod
+	def to_py_dict(cls, data):
+		return json.loads(data)
+
+@app.exception_handler(Exception)
+async def exceptionHandler(request, exc):
+	return JSONResponse(
+		status_code = 500,
+		content = { "access_control": "failure", "error_message": "Unexpected error occured." },
+	)
+
 @app.get("/data/access-control")
-def accessControl():
-	return { "access_control": "success" }
+async def accessControl(data: Data):
+	isAllowed = await checkIfUserAllowed("app7ACL.db", data.user_id, data.role.value)
+	return { "access_control": "success", "is_allowed": isAllowed }
